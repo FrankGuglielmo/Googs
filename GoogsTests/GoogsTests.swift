fc5bc0345f54f503f2a722d00c82ed4eb416f45c
Frank Guglielmo <frankieg1610@gmail.com>
2025-04-10 23:14:23 -0700

    Initial Commit

---
+++ GoogsTests/GoogsTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoogsTests.swift
+//  GoogsTests
+//
+//  Created by Frank Guglielmo on 4/10/25.
+//
+
+import Testing
+@testable import Googs
+
+struct GoogsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
